73199599c8e2d30897392cd76f6d5f2839cbb736
Yoann Scherrer <yoannscherrer@Host-003.lan>
2025-01-14 21:23:21 +0100

    Visuel Page Home terminé

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,13 @@
 import '../styles/card.scss'
+
+function Card({ cover, title}) {
+    return (
+        <div className="card" style={{ backgroundImage: `url(${cover})` }}>
+            <div className="card-details">
+                <h3>{title}</h3>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,27 @@
+import Card from './Card'; 
+import logements from '../logements.json'; 
+import '../styles/home.scss'; 
+
 function Home() {
-    return <h1>Page d'accueil</h1>;
-  }
-  
-  export default Home;
+    return (
+        <div className="home">
+            <section className="first-section">
+            <p className="first-section-text">Chez vous, partout et ailleurs</p>
+            </section>
+
+            <section className="second-section">
+                <div className="card-list">
+                    {logements.map((logement) => (
+                        <Card
+                            key={logement.id}
+                            cover={logement.cover}
+                            title={logement.title}
+                        />
+                    ))}
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Home;
